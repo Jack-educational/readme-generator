ef516043380d499cc03830ef7091aeccb5f12f1c
Jack Turner <34825609+neverekt@users.noreply.github.com>
2023-02-28 15:13:59 +0000

    created index.js to import inquirer for the user prompt chain, fileststem for outputting to a file, util and the markdown generation module which is in the same folder

---
+++ index.js
@@ -0,0 +1,63 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Please enter a brief description of your project:',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Please enter installation instructions:',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Please enter usage information:',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Please enter contribution guidelines:',
+      name: 'contributing',
+    },
+    {
+      type: 'input',
+      message: 'Please enter test instructions:',
+      name: 'tests',
+    },
+    {
+      type: 'list',
+      message: 'Please choose a license for your application:',
+      name: 'license',
+      choices: ['MIT', 'GPL', 'Apache'],
+    },
+    {
+      type: 'input',
+      message: 'Please enter your GitHub username:',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'Please enter your email address:',
+      name: 'email',
+    },
+  ])
+  .then((data) => {
+    // generate markdown file using user input
+    const markdown = generateMarkdown(data);
+
+    // write markdown file
+    fs.writeFile('README.md', markdown, (err) =>
+      err ? console.error(err) : console.log('README.md file created!')
+    );
+  });
