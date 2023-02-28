627bae4b39dccdb7897189a39c152b7ded8da4e5
Jack Turner <34825609+neverekt@users.noreply.github.com>
2023-02-28 15:55:06 +0000

    fixed require path for generateMarkdown

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
-const generateMarkdown = require('./utils/generateMarkdown');
+const util =require('util');
+const generateMarkdown = require('./generateMarkdown');
 
 inquirer
   .prompt([
